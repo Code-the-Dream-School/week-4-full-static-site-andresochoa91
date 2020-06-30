17bb989541cdcb158017a67af0beef886ef62936
andresochoa91 <andres_ochoa91@hotmail.com>
2020-06-30 15:06:38 -0700

    some optimization with app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+let cuisines = ["Italian", "American", "Korean", "Thai", "Caribbean", "Chinese", "Latin American", "Spanish", "Italian", "Japanese"];
+
+let cuisinesTable = "";
+
+for (let i = 0; i < cuisines.length; i++) {
+  cuisinesTable += `
+    <tr>
+      <th scope="row">${i + 1}</th>
+      <td>${cuisines[i]}</td>
+      <td><a href="">link</a></td>
+    </tr>
+  `;
+}
+
+let more = `
+  <tr>
+    <th scope="row"></th>
+    <td><a href="#">More here</a></td>
+    <td></td>
+  </tr>
+`;
+
+let tbody = document.querySelector("tbody")
+tbody.innerHTML = cuisinesTable += more;
