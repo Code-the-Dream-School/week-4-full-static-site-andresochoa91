45c94a32751548fccf2b96085a7d418e1f6dc238
andresochoa91 <andres_ochoa91@hotmail.com>
2020-06-30 16:33:00 -0700

    ready

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-let cuisines = ["Italian", "American", "Korean", "Thai", "Caribbean", "Chinese", "Latin American", "Spanish", "Italian", "Japanese"];
+$("video").mediaelementplayer({
+  features: ["playpause", "progress", "tracks", "volume", "fullscreen"],
+  // startLanguage: "en"  /* bug */
+});
 
-let cuisinesTable = "";
 
-for (let i = 0; i < cuisines.length; i++) {
-  cuisinesTable += `
+(function () {
+  let cuisines = ["Italian", "American", "Korean", "Thai", "Caribbean", "Chinese", "Latin American", "Spanish", "Italian", "Japanese"];
+  let cuisinesTable = "";
+  
+  for (let i = 0; i < cuisines.length; i++) {
+    cuisinesTable += `
+      <tr>
+        <th scope="row">${i + 1}</th>
+        <td>${cuisines[i]}</td>
+        <td><a href="">link</a></td>
+      </tr>
+    `;
+  }
+  
+  let more = `
     <tr>
-      <th scope="row">${i + 1}</th>
-      <td>${cuisines[i]}</td>
-      <td><a href="">link</a></td>
+      <th scope="row"></th>
+      <td><a href="#">More here</a></td>
+      <td></td>
     </tr>
   `;
-}
-
-let more = `
-  <tr>
-    <th scope="row"></th>
-    <td><a href="#">More here</a></td>
-    <td></td>
-  </tr>
-`;
-
-let tbody = document.querySelector("tbody")
-tbody.innerHTML = cuisinesTable += more;
+  
+  let tbody = document.querySelector("tbody")
+  tbody.innerHTML = cuisinesTable += more;
+})();
